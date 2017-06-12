6fbf9c39080002d24418c0688ed1f6578dc5c43c
Summer <victoriasqq@gmail.com>
2017-06-12 10:00:39 +0800

    trace telemetry data in simulated device (#1)
    * trace telemetry data in simulated device
    
    * add opt in before sending telemetry data
    
    * throw exception after trace telemetry when something went wrong and set default telemetry switch empty

--- SimulatedDevice/Program.cs
+++ SimulatedDevice/Program.cs
@@ -1,18 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Azure.Devices.Client;
-using Newtonsoft.Json;
-
-namespace SimulatedDevice
+﻿namespace SimulatedDevice
 {
+    using System;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Devices.Client;
+    using Newtonsoft.Json;
+    using Telemetry;
+
     class Program
     {
-        static DeviceClient deviceClient;
-        static string iotHubUri = "{iot hub hostname}";
-        static string deviceKey = "{device key}";
+        private static DeviceClient _deviceClient;
+        private const string IoTHubUri = "{iot hub hostname}";
+        private const string DeviceKey = "{device key}";
+
+        private const string DeviceId = "myFirstDevice";
+        private const string Name = "simulateddevice";
+
+        private static void Main(string[] args)
+        {
+            Telemetry.AskForPermission();
+            Console.WriteLine("Simulated device\n");
+            try
+            {
+                _deviceClient = DeviceClient.Create(IoTHubUri,new DeviceAuthenticationWithRegistrySymmetricKey(DeviceId, DeviceKey), TransportType.Mqtt);
+            }
+            catch (Exception e)
+            {
+                Telemetry.Instance.Track("failed", IoTHubUri, Name, $"create device client failed: {e.Message}");
+                throw;
+            }
+            Telemetry.Instance.Track("success", IoTHubUri, Name, "device client created");
+            SendDeviceToCloudMessagesAsync();
+            Console.ReadLine();
+        }
 
         private static async void SendDeviceToCloudMessagesAsync()
         {
@@ -20,37 +40,33 @@ namespace SimulatedDevice
             double minHumidity = 60;
             int messageId = 1;
             Random rand = new Random();
-
-            while (true)
+            try
             {
-                double currentTemperature = minTemperature + rand.NextDouble() * 15;
-                double currentHumidity = minHumidity + rand.NextDouble() * 20;
-
-                var telemetryDataPoint = new
+                while (true)
                 {
-                    messageId = messageId++,
-                    deviceId = "myFirstDevice",
-                    temperature = currentTemperature,
-                    humidity = currentHumidity
-                };
-                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
-                var message = new Message(Encoding.ASCII.GetBytes(messageString));
-                message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
+                    double currentTemperature = minTemperature + rand.NextDouble() * 15;
+                    double currentHumidity = minHumidity + rand.NextDouble() * 20;
 
-                await deviceClient.SendEventAsync(message);
-                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+                    var telemetryDataPoint = new
+                    {
+                        messageId = messageId++,
+                        deviceId = DeviceId,
+                        temperature = currentTemperature,
+                        humidity = currentHumidity
+                    };
+                    var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                    var message = new Message(Encoding.ASCII.GetBytes(messageString));
+                    message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
 
-                await Task.Delay(1000);
+                    await _deviceClient.SendEventAsync(message);
+                    Console.WriteLine($"{DateTime.Now} > Sending message: {messageString}");
+                    await Task.Delay(1000);
+                }
             }
-        }
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Simulated device\n");
-            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey("myFirstDevice", deviceKey), TransportType.Mqtt);
-
-            SendDeviceToCloudMessagesAsync();
-            Console.ReadLine();
+            catch (Exception e)
+            {
+                Telemetry.Instance.Track("failed", IoTHubUri, Name, $"send message to cloud exception: {e.Message}");
+            }
         }
     }
 }
---
+++ Telemetry/Telemetry.cs
@@ -0,0 +1,144 @@
+﻿namespace Telemetry
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Net.NetworkInformation;
+    using System.Security.Cryptography;
+    using System.Text;
+    using System.Xml.Linq;
+    using Microsoft.ApplicationInsights;
+
+    public sealed class Telemetry
+    {
+        private static readonly Lazy<Telemetry> Lazy = new Lazy<Telemetry>(() => new Telemetry());
+        public static Telemetry Instance => Lazy.Value;
+        private static readonly string ConfigFilePath = GetTelemetryConfigPath();
+        private static readonly XDocument Doc = XDocument.Load(ConfigFilePath);
+        private const string ConfigFileName = "telemetry.config";
+        private const string TelemetryKey = "telemetry";
+        private const string InstrumentationKey = "instrumentationKey";
+        private const string SimulatedDevice = "simulated device";
+
+        private const string PromptText =
+            "Microsoft would like to collect data about how users use Azure IoT samples and some problems they encounter.\r\n" +
+            "Microsoft uses this information to improve our tooling experience.\r\n" +
+            "Participation is voluntary and when you choose to participate, your device will automatically sends information to Microsoft about how you use Azure IoT samples";
+
+        private static readonly TelemetryClient Client = new TelemetryClient
+        {
+            InstrumentationKey = ReadConfig(InstrumentationKey)
+        };
+
+        private static string GetTelemetryConfigPath()
+        {
+            var projectPath = Directory.GetParent(Environment.CurrentDirectory).Parent;
+            var temp = projectPath;
+            var configPath = "";
+            do
+            {
+                configPath = Path.Combine(temp.Parent.FullName, ConfigFileName);
+            } while (!File.Exists(configPath) && temp.Parent != null);
+
+            return configPath;
+        }
+
+        private static string ReadConfig(string key)
+        {
+            if (!string.IsNullOrEmpty(ConfigFilePath))
+            {
+                return Doc.Descendants(key).First()?.Value;
+            }
+            return null;
+        }
+
+        private static void SetConfig(string key, string value)
+        {
+            if (!string.IsNullOrEmpty(ConfigFilePath))
+            {
+                var element = Doc.Descendants(key).First();
+                if (element != null)
+                {
+                    element.Value = value;
+                    Doc.Save(ConfigFilePath);
+                }
+            }
+        }
+
+        public static void AskForPermission()
+        {
+            if (!string.IsNullOrEmpty(ReadConfig(TelemetryKey)))
+            {
+                return;
+            }
+            string response;
+            Console.WriteLine(PromptText);
+            do
+            {
+                Console.Write("Select y to enable data collection :(y/n, default is y) ");
+                response = Console.ReadLine();
+            } while (response != "" && response.ToLower() != "y" && response.ToLower() != "n");
+            SetConfig(TelemetryKey, (response == "" || response.ToLower() == "y") ? "true" : "false");
+            if (response.ToLower() == "false")
+            {
+                Instance.TrackSimpleSuccess();
+            }
+        }
+
+        private static string SHA256Hash(string value)
+        {
+            using (var hash = SHA256.Create())
+            {
+                return string.Concat(hash.ComputeHash(Encoding.UTF8.GetBytes(value)).Select(item => item.ToString("x2")));
+            }
+        }
+
+        private static string GetMac()
+        {
+            return
+                NetworkInterface.GetAllNetworkInterfaces()
+                    .Where(p => p.OperationalStatus == OperationalStatus.Up)
+                    .Select(p => p.GetPhysicalAddress().ToString())
+                    .FirstOrDefault();
+        }
+
+        private void TrackSimpleSuccess()
+        {
+            Client.TrackEvent("success", new Dictionary<string, string>
+            {
+                {"language", "C#"},
+                {"device", SimulatedDevice},
+                {"osversion", Environment.OSVersion.ToString()},
+                {"mac", SHA256Hash(GetMac())}
+            });
+        }
+
+        public void Track(string eventName, string connectionstring, string projectName, string message)
+        {
+            bool telemetrySwitch;
+            bool.TryParse(ReadConfig(TelemetryKey), out telemetrySwitch);
+            if (!telemetrySwitch)
+            {
+                return;
+            }
+            try
+            {
+                Client.TrackEvent(eventName, new Dictionary<string, string>
+                {
+                    {"iothub", SHA256Hash(connectionstring.Split('.').FirstOrDefault())},
+                    {"language", "C#"},
+                    {"device", SimulatedDevice},
+                    {"project_name", projectName},
+                    {"osversion", Environment.OSVersion.ToString()},
+                    {"mac", SHA256Hash(GetMac())},
+                    {"message", message}
+                });
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+    }
+}
