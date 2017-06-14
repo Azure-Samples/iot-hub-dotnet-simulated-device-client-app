6495861a59072c11e288fc63f6bd82c4740ea8b2
Qingqing Sun <qisun@microsoft.com>
2017-06-14 14:27:51 +0800

    remove telemetry in sim and readd2cmessage

--- ReadDeviceToCloudMessages/Program.cs
+++ ReadDeviceToCloudMessages/Program.cs
@@ -5,8 +5,7 @@
     using System.Text;
     using Microsoft.ServiceBus.Messaging;
     using System.Threading;
-    using System.Collections.Generic;
-    using Telemetry;
+    using System.Linq;
 
     class Program
     {
@@ -14,45 +13,26 @@
         private const string IotHubD2CEndpoint = "messages/events";
         private static EventHubClient _eventHubClient;
 
-        private const string Name = "readdevicetocloudmessages";
-
         private static async Task ReceiveMessagesFromDeviceAsync(string partition, CancellationToken ct)
         {
             var eventHubReceiver = _eventHubClient.GetDefaultConsumerGroup().CreateReceiver(partition, DateTime.UtcNow);
-            try
+            while (true)
             {
-                while (true)
-                {
-                    if (ct.IsCancellationRequested) break;
-                    var eventData = await eventHubReceiver.ReceiveAsync();
-                    if (eventData == null) continue;
-                    var data = Encoding.UTF8.GetString(eventData.GetBytes());
-                    Console.WriteLine($"Message received. Partition: {partition} Data: '{data}'");
-                }
-            }
-            catch (Exception e)
-            {
-                Telemetry.Instance.Track("failed", ConnectionString, Name, $"receive device message exception: {e.Message}");
+                if (ct.IsCancellationRequested) break;
+                var eventData = await eventHubReceiver.ReceiveAsync();
+                if (eventData == null) continue;
+
+                var data = Encoding.UTF8.GetString(eventData.GetBytes());
+                Console.WriteLine("Message received. Partition: {0} Data: '{1}'", partition, data);
             }
         }
+
         static void Main(string[] args)
         {
-            Telemetry.AskForPermission();
             Console.WriteLine("Receive messages. Ctrl-C to exit.\n");
-            try
-            {
-                _eventHubClient = EventHubClient.CreateFromConnectionString(ConnectionString, IotHubD2CEndpoint);
-            }
-            catch (Exception e)
-            {
-                Telemetry.Instance.Track("failed", ConnectionString, Name, $"event hub client failed: {e.Message}");
-                throw;
-            }
-
-            Telemetry.Instance.Track("success", ConnectionString, Name, "event hub client created");
-            var d2cPartitions = _eventHubClient.GetRuntimeInformation().PartitionIds;
-
-            CancellationTokenSource cts = new CancellationTokenSource();
+            _eventHubClient = EventHubClient.CreateFromConnectionString(ConnectionString, IotHubD2CEndpoint);
+            var partitions = _eventHubClient.GetRuntimeInformation().PartitionIds;
+            var cts = new CancellationTokenSource();
 
             Console.CancelKeyPress += (s, e) =>
             {
@@ -61,11 +41,7 @@
                 Console.WriteLine("Exiting...");
             };
 
-            var tasks = new List<Task>();
-            foreach (string partition in d2cPartitions)
-            {
-                tasks.Add(ReceiveMessagesFromDeviceAsync(partition, cts.Token));
-            }
+            var tasks = partitions.Select(partition => ReceiveMessagesFromDeviceAsync(partition, cts.Token)).ToList();
             Task.WaitAll(tasks.ToArray());
         }
     }
--- SimulatedDevice/Program.cs
+++ SimulatedDevice/Program.cs
@@ -5,34 +5,13 @@
     using System.Threading.Tasks;
     using Microsoft.Azure.Devices.Client;
     using Newtonsoft.Json;
-    using Telemetry;
 
     class Program
     {
-        private static DeviceClient _deviceClient;
-        private const string IoTHubUri = "{iot hub hostname}";
-        private const string DeviceKey = "{device key}";
-
-        private const string DeviceId = "myFirstDevice";
-        private const string Name = "simulateddevice";
-
-        private static void Main(string[] args)
-        {
-            Telemetry.AskForPermission();
-            Console.WriteLine("Simulated device\n");
-            try
-            {
-                _deviceClient = DeviceClient.Create(IoTHubUri,new DeviceAuthenticationWithRegistrySymmetricKey(DeviceId, DeviceKey), TransportType.Mqtt);
-            }
-            catch (Exception e)
-            {
-                Telemetry.Instance.Track("failed", IoTHubUri, Name, $"create device client failed: {e.Message}");
-                throw;
-            }
-            Telemetry.Instance.Track("success", IoTHubUri, Name, "device client created");
-            SendDeviceToCloudMessagesAsync();
-            Console.ReadLine();
-        }
+        static DeviceClient deviceClient;
+        static string iotHubUri = "{iot hub hostname}";
+        static string deviceKey = "{device key}";
+        private static string DeviceId = "myFirstDevice";
 
         private static async void SendDeviceToCloudMessagesAsync()
         {
@@ -40,33 +19,37 @@
             double minHumidity = 60;
             int messageId = 1;
             Random rand = new Random();
-            try
+
+            while (true)
             {
-                while (true)
+                double currentTemperature = minTemperature + rand.NextDouble() * 15;
+                double currentHumidity = minHumidity + rand.NextDouble() * 20;
+
+                var telemetryDataPoint = new
                 {
-                    double currentTemperature = minTemperature + rand.NextDouble() * 15;
-                    double currentHumidity = minHumidity + rand.NextDouble() * 20;
+                    messageId = messageId++,
+                    deviceId = DeviceId,
+                    temperature = currentTemperature,
+                    humidity = currentHumidity
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+                message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
 
-                    var telemetryDataPoint = new
-                    {
-                        messageId = messageId++,
-                        deviceId = DeviceId,
-                        temperature = currentTemperature,
-                        humidity = currentHumidity
-                    };
-                    var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
-                    var message = new Message(Encoding.ASCII.GetBytes(messageString));
-                    message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
 
-                    await _deviceClient.SendEventAsync(message);
-                    Console.WriteLine($"{DateTime.Now} > Sending message: {messageString}");
-                    await Task.Delay(1000);
-                }
+                await Task.Delay(1000);
             }
-            catch (Exception e)
-            {
-                Telemetry.Instance.Track("failed", IoTHubUri, Name, $"send message to cloud exception: {e.Message}");
-            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simulated device\n");
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(DeviceId, deviceKey), TransportType.Mqtt);
+
+            SendDeviceToCloudMessagesAsync();
+            Console.ReadLine();
         }
     }
 }
